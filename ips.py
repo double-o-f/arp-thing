04bf4c3af52ff9aa6fe1d8a765be509377734d73
Sam <applepatato@gmail.com>
2024-11-24 15:39:39 -0600

    get host names correctly

--- ips.py
+++ ips.py
@@ -3,7 +3,7 @@ open("ips.txt", "w").close
 ips = open("ips.txt", "a")
 
 for i in arp:
-   if (i[:3] == "192" or i[:2] == "172" or i[:2] == "10."):
+   if (i[:3] == "192" or i[:3] == "172" or i[:3] == "10."):
       ips.write(i[:i.find(chr(9))] + "\n")
 arp.close()
 ips.close()
